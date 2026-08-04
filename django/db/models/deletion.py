614764db8ee0cc792f0312e0134a79c7bcd2bd47
Chen shixian <chensx20@users.noreply.github.com>
2026-08-04 11:46:10 +0000

    refactor(deletion): keep batch fields strict

--- django/db/models/deletion.py
+++ django/db/models/deletion.py
@@ -257,8 +257,6 @@ class Collector:
         """
         Return the objs in suitably sized batches for the used connection.
         """
-        if not isinstance(fields, list):
-            fields = [fields]
         conn_batch_size = max(
             connections[self.using].ops.bulk_batch_size(fields, objs), 1
         )
