bcccd8a503d894388e9b819b06c9bd48c77e5d33
Casey Brooks <casey.brooks@users.noreply.github.com>
2026-08-07 02:03:12 +0000

    Fixed #30577 -- Rendered readonly admin JSONField values as JSON.

--- tests/admin_utils/tests.py
+++ tests/admin_utils/tests.py
@@ -197,6 +197,21 @@ class UtilsTests(SimpleTestCase):
         display_value = display_for_field(12345, models.IntegerField(), self.empty_value)
         self.assertEqual(display_value, '12,345')
 
+    def test_json_display_for_field(self):
+        tests = [
+            ({'a': {'b': 'c'}}, '{"a": {"b": "c"}}'),
+            (['a', 'b'], '["a", "b"]'),
+            ('a', '"a"'),
+            ({'a': '你好 世界'}, '{"a": "你好 世界"}'),
+            ({('a', 'b'): 'c'}, "{('a', 'b'): 'c'}"),
+        ]
+        for value, display_value in tests:
+            with self.subTest(value=value):
+                self.assertEqual(
+                    display_for_field(value, models.JSONField(), self.empty_value),
+                    display_value,
+                )
+
     def test_list_display_for_value(self):
         display_value = display_for_value([1, 2, 3], self.empty_value)
         self.assertEqual(display_value, '1, 2, 3')
